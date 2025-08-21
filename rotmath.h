145d50ea442459b2a801e67f5c28684c8337daa9
codisafish <codisafish@192.168.1.14>
2025-08-21 19:31:16 +0200

    rotmath cleanup

--- rotmath.h
+++ rotmath.h
@@ -5,56 +5,58 @@ typedef double** mat2x2;
 typedef double*  vec2;
 
 mat2x2 new_mat2x2(){
-  double* o = (double*)malloc(sizeof(double) * 2);
-  double* i = (double*)malloc(sizeof(double) * 2);
+  double* O = (double*)malloc(sizeof(double) * 2);
+  double* I = (double*)malloc(sizeof(double) * 2);
   mat2x2 result = (mat2x2)malloc(sizeof(double*) * 2);
-  result[0] = o;
-  result[1] = i;
+  result[0] = O;
+  result[1] = I;
   return result;
 }
 
-vec2 new_vec2(){
+vec2 vector(double x, double y){
   vec2 result = (vec2)malloc(sizeof(double) * 2);
-  result[0] = 1;
-  result[1] = 0;
+  result[0] = x;
+  result[1] = y;
   return result;
 }
 
-mat2x2 new_rot_matrix(double phi){
-  mat2x2 result = new_mat2x2();
-  result[0][0] = cos(phi);
-  result[0][1] = sin(phi) * -1;
-  result[1][0] = sin(phi);
-  result[1][1] = cos(phi);
-  return result;
-}
-
-void multiply_mxm(mat2x2 first, mat2x2 second, mat2x2* result){
-  double OO = (first[0][0] * second[0][0]) + (first[0][1] * second[1][0]);
-  double OI = (first[0][0] * second[0][1]) + (first[0][1] * second[1][1]);
-  double IO = (first[1][0] * second[0][0]) + (first[1][1] * second[1][0]);
-  double II = (first[1][0] * second[0][1]) + (first[1][1] * second[1][1]);
-  (*result)[0][0] = OO; (*result)[1][0] = IO; (*result)[0][1] = OI; (*result)[1][1] = II;
-}
-
-void multiply_mxv(mat2x2 first, vec2 second, vec2* result){
-  double O = (first[0][0] * second[0]) + (first[0][1] * second[1]);
-  double I = (first[1][0] * second[0]) + (first[1][1] * second[1]);
-  (*result)[0] = O; (*result)[1] = I; 
-}
-
-void reverse_matrix(mat2x2* mat){
-  (*mat)[0][1] = (*mat)[0][1] * -1;
-  (*mat)[1][0] = (*mat)[1][0] * -1;
-}
-
-void free2x2mat(mat2x2 mat){
+void delete_2x2mat(mat2x2 mat){
   free(mat[0]);
   free(mat[1]);
   free(mat);
 }
 
-void free_vec2(vec2 vec){
+void delete_vec2(vec2 vec){
   free(vec);
 }
 
+void matrix(double phi, mat2x2* target){
+  (*target)[0][0] = cos(phi);
+  (*target)[0][1] = sin(phi) * -1;
+  (*target)[1][0] = sin(phi);
+  (*target)[1][1] = cos(phi);
+}
+
+void compose(mat2x2 first, double phi, mat2x2* result){
+  mat2x2 temp = new_mat2x2();
+  get_rotation_matrix(phi, &temp);
+
+  double OO = (first[0][0] * temp[0][0]) + (first[0][1] * temp[1][0]);
+  double OI = (first[0][0] * temp[0][1]) + (first[0][1] * temp[1][1]);
+  double IO = (first[1][0] * temp[0][0]) + (first[1][1] * temp[1][0]);
+  double II = (first[1][0] * temp[0][1]) + (first[1][1] * temp[1][1]);
+  (*result)[0][0] = OO; (*result)[1][0] = IO; (*result)[0][1] = OI; (*result)[1][1] = II;
+
+  delete_2x2mat(temp);
+}
+
+void rotate_vec(mat2x2 first, vec2 second, vec2* result){
+  double O = (first[0][0] * second[0]) + (first[0][1] * second[1]);
+  double I = (first[1][0] * second[0]) + (first[1][1] * second[1]);
+  (*result)[0] = O; (*result)[1] = I; 
+}
+
+void inverse_matrix(mat2x2* mat){
+  (*mat)[0][1] = (*mat)[0][1] * -1;
+  (*mat)[1][0] = (*mat)[1][0] * -1;
+}
